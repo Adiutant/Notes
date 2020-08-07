1beaa0c0392486abcc0ead9aa234ac8c351e552f
Adiutant <lagynkov.rom@gmail.com>
2020-08-07 22:14:18 +0300

    Cleared code and view change started

--- app/src/main/java/com/adiutant/notes/activities/MainActivity.kt
+++ app/src/main/java/com/adiutant/notes/activities/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.ContextMenu
 import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView.AdapterContextMenuInfo
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.adiutant.notes.R
@@ -18,52 +17,46 @@ import com.google.android.material.tabs.TabItem
 import com.google.android.material.tabs.TabLayout
 
 
-//import com.arellomobile.mvp.presenter.InjectPresenter
 
 
 class MainActivity : AppCompatActivity(), MainView {
-    //private lateinit var adapter: ArrayAdapter<Notes>
     private lateinit var _listView: RecyclerView
     private lateinit var _tabLayout: TabLayout
-    private lateinit var _newNote: TabItem
     private  var contextPosition:Int =0
     lateinit var presenter: MainPresenter
     private var isCreate = false
-
-    private lateinit var allNotes: ArrayList<Notes>
-    //private lateinit var dbHelper: DBHelper
-    //private var currentUser = User()
+    private val DELETE_NOTE_CODE = "Удалить"
+    private val NOTE_RUS_CODE ="Заметка"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         presenter = MainPresenter()
         _listView = findViewById(R.id.taskList)
-        _tabLayout = findViewById(R.id.tabLayout)
+      //  _tabLayout = findViewById(R.id.tabLayout)
+      //  _tabLayout.
+
         onNotesLoaded(presenter.loadAllNotes())
         with(ItemClickSupport.addTo(_listView)) {
             setOnItemClickListener { _, position, _ -> openNoteScreen(presenter.openNote(position)) }
             setOnItemLongClickListener { _, position, _ -> contextPosition = position
                 openContextMenu(_listView); true }
         }
-        _tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                isCreate=true
-                openNoteScreen(presenter.openNote(-1))
-            }
+//        _tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+//            override fun onTabSelected(tab: TabLayout.Tab?) {
+//                isCreate=true
+//                openNoteScreen(presenter.openNote(-1))
+//            }
+//
+//            override fun onTabReselected(tab: TabLayout.Tab?) {
+//                // Handle tab reselect
+//            }
+//
+//            override fun onTabUnselected(tab: TabLayout.Tab?) {
+//                // Handle tab unselect
+//            }
+//        })
 
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                // Handle tab reselect
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                // Handle tab unselect
-            }
-        })
-
-        //listNotes.add(Notes("Note one",Date(System.currentTimeMillis()),Date(System.currentTimeMillis())))
-        //listNotes.add(Notes("Note two",Date(System.currentTimeMillis()),Date(System.currentTimeMillis())))
-        //listNotes.add(Notes("Note three",Date(System.currentTimeMillis()),Date(System.currentTimeMillis())))
     }
     override fun onNotesLoaded(notes: List<Notes>) {
         _listView.adapter= RecAdapter(notes)
@@ -77,12 +70,12 @@ class MainActivity : AppCompatActivity(), MainView {
         menuInfo: ContextMenu.ContextMenuInfo?
     ) {
         super.onCreateContextMenu(menu, v, menuInfo)
-        menu!!.setHeaderTitle("Заметка")
-        menu.add(0,v!!.id,0,"Удалить")
+        menu!!.setHeaderTitle(NOTE_RUS_CODE)
+        menu.add(0,v!!.id,0,DELETE_NOTE_CODE)
     }
 
     override fun onContextItemSelected(item: MenuItem): Boolean {
-        if (item!!.title =="Удалить")
+        if (item!!.title ==DELETE_NOTE_CODE)
         {
             presenter.deleteNote(contextPosition+1)
             onNoteDeleted()
@@ -122,12 +115,5 @@ class MainActivity : AppCompatActivity(), MainView {
         intent.putExtras(bundle)
         startActivity(intent)
     }
-
-
-//    private fun act(view: View)
-//    {
-//        var intent = Intent(this, AddNote::class.java)
-//        startActivity(intent)
-//    }
 
 }
